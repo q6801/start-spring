f44df4fa82364706be978030ebd9723c0d484bfd
Lee SeungHun <q6801@naver.com>
2021-07-20 11:15:39 +0900

    feat : Use singleton pattern

---
+++ hello-java/core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,23 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
